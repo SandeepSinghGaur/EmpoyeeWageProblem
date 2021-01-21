3c825d75762c7d7d92f116218a8297257e2895c3
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-21 21:50:05 +0530

    [Sandeep] Added:Daily Wage By Using Switch

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,25 +4,39 @@ class EmployeeWage
     static void Main(string[] args)
     {
         Console.WriteLine("Calculate Daily Employee Wage");
-        int IS_PRESENT = 1;
-        int IS_FULL_TIME = 1;
-        int WAGE_PER_HOUR = 20;
+        int WAGE_PER__HOUR = 20;
         int total_hour = 0;
         Random random = new Random();
         int check_Attandance = random.Next(0, 2);
         int calculate_hour = random.Next(0, 2);
-        if (check_Attandance ==IS_PRESENT)
+        switch (check_Attandance)
         {
-            if (calculate_hour ==IS_FULL_TIME)
-                total_hour = 8;
-            else
-                total_hour = 4;
+            case 0:
+                Console.WriteLine("NO ! Employess is Not Present");
+                break;
+            case 1:
+                switch (calculate_hour)
+                {
+                    case 0:
+                        Console.WriteLine("Today Employee Works half time");
+                        total_hour = 4;
+                        break;
+                    case 1:
+                        Console.WriteLine("Today Employee Works Full time");
+                        total_hour = 8;
+                        break;
+                    default:
+                        break;
+
+                }
+                int Wage = total_hour * WAGE_PER__HOUR;
+                Console.WriteLine("Employee Daily Wage" + " " + Wage);
+                break;
+            default:
+                break;
         }
-        else
-        {
-            Console.WriteLine("NO ! Employess is Not Present");
-        }
-        int Wage = total_hour * WAGE_PER_HOUR;
-        Console.WriteLine("Employee Daily Wage" + " " + Wage);
+
     }
+
 }
+
